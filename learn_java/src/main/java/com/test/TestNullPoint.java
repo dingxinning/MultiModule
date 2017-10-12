8e2e4ee7963b915ada72feec315796ce09f7c26d
yxrswx <yxrswx@163.com>
2017-10-12 16:31:03 +0800

    1

--- learn_java/src/main/java/com/test/TestNullPoint.java
+++ learn_java/src/main/java/com/test/TestNullPoint.java
@@ -2,9 +2,11 @@ package com.test;
 
 /**
  * Created by Wuxiang on 2017/9/11.
+ *
+ * 测试  null point exception
  */
 public class TestNullPoint {
-    public static void main(String[] args) {
+    public static void main(String[] args){
         String aaa = null;
         if (aaa!=null && !(aaa.trim().isEmpty())) {
             System.out.println("add");
