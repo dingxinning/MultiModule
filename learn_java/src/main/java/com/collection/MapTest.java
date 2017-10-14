a2c7a2aff33c54f83e5090c07793632b6e1d0e22
yxrswx <yxrswx@163.com>
2017-10-15 01:15:17 +0800

    map

--- learn_java/src/main/java/com/collection/MapTest.java
+++ learn_java/src/main/java/com/collection/MapTest.java
@@ -1,13 +1,11 @@
 package com.collection;
 
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.Map;
+import java.util.*;
 import java.util.Map.Entry;
-import java.util.Set;
 
 /**
  * Created by Administrator on 2016/12/29.
+ * Map 允许插入空值
  */
 public class MapTest {
     public static void main(String [] args){
@@ -15,6 +13,7 @@ public class MapTest {
         map.put("id", "1");
         map.put("name", "wx");
         map.put("age", "18");
+        map.put("age", "19"); // 从来只闻新人笑，哪里识得旧人哭
         map.put("sex", "man");
         map.remove("name");
         map.put(null, "空");
@@ -24,12 +23,12 @@ public class MapTest {
             System.out.print(entry.getKey()+": ");
             System.out.print(entry.getValue()+".  ");
         }
-        System.out.println("\n");
+        System.out.println("");
 
         for (String key : map.keySet()) {
             System.out.print(map.get(key)+" ,");
         }
-        System.out.println("\n");
+        System.out.println("");
 
         Iterator<Entry<String, String>> iterator = map.entrySet().iterator();
         while (iterator.hasNext()) {
@@ -37,12 +36,23 @@ public class MapTest {
             System.out.print(entry.getKey()+": ");
             System.out.print(entry.getValue()+".  ");
         }
-        System.out.println("\n");
+        System.out.println("");
 
         Set<Entry<String, String>> entrySet = map.entrySet();
         for (Entry<String, String> entry : entrySet) {
             System.out.print(entry.getKey()+": ");
             System.out.print(entry.getValue()+".  ");
         }
+        System.out.println();
+
+        // Map 实现对随机数进行计数
+        Random rand = new Random(47);
+        Map<Integer,Integer> m = new HashMap<>();
+        for(int i = 0; i < 10000; i++) {
+            int r = rand.nextInt(20);
+            Integer freq = m.get(r);
+            m.put(r, freq == null ? 1 : freq + 1);
+        }
+        System.out.println(m);
     }
 }
