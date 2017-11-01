4d657e05113643b116bf1def05cb0583b680db9c
yxrswx <yxrswx@163.com>
2017-11-01 18:07:53 +0800

    object  2 map

--- learn_java/src/main/java/com/utils/Object2Map.java
+++ learn_java/src/main/java/com/utils/Object2Map.java
@@ -3,7 +3,11 @@ package com.utils;
 import com.cglib.LeapRole;
 import com.mongodb.util.JSON;
 
+import java.beans.BeanInfo;
+import java.beans.Introspector;
+import java.beans.PropertyDescriptor;
 import java.lang.reflect.Field;
+import java.lang.reflect.Method;
 import java.lang.reflect.Modifier;
 import java.util.HashMap;
 import java.util.Map;
@@ -12,6 +16,12 @@ import java.util.Map;
  * Created by Wuxiang on 2017/11/1.
  * Object 与 Map 之间的转换
  * 仅限于 Object 拥有java的实体类结构
+ *
+ * 参考：http://www.open-open.com/code/view/1423280939826
+ *
+ * 方法1 利用 beanutils
+ * 方法2 利用 Introspector 进行转换
+ * 方法3 利用 反射
  */
 public class Object2Map {
     public static void main(String[] args) throws Exception {
@@ -21,14 +31,71 @@ public class Object2Map {
         Object object = (Object) leapRole;
         System.out.println(object);
 
-        Map<String, Object> map = objectToMap(object);
+        Map<String, Object> map = objectToMap3(object);
         System.out.println(map);
 
         String json = JSON.serialize(map);
         System.out.println(json);
     }
 
-    public static Object mapToObject(Map<String, Object> map, Class<?> beanClass) throws Exception {
+    public static Object mapToObject1(Map<String, Object> map, Class<?> beanClass) throws Exception {
+        if (map == null)
+            return null;
+
+        Object obj = beanClass.newInstance();
+
+        org.apache.commons.beanutils.BeanUtils.populate(obj, map);
+
+        return obj;
+    }
+
+    public static Map<?, ?> objectToMap1(Object obj) {
+        if(obj == null)
+            return null;
+
+        return new org.apache.commons.beanutils.BeanMap(obj);
+    }
+
+    public static Object mapToObject2(Map<String, Object> map, Class<?> beanClass) throws Exception {
+        if (map == null)
+            return null;
+
+        Object obj = beanClass.newInstance();
+
+        BeanInfo beanInfo = Introspector.getBeanInfo(obj.getClass());
+        PropertyDescriptor[] propertyDescriptors = beanInfo.getPropertyDescriptors();
+        for (PropertyDescriptor property : propertyDescriptors) {
+            Method setter = property.getWriteMethod();
+            if (setter != null) {
+                setter.invoke(obj, map.get(property.getName()));
+            }
+        }
+
+        return obj;
+    }
+
+    public static Map<String, Object> objectToMap2(Object obj) throws Exception {
+        if(obj == null)
+            return null;
+
+        Map<String, Object> map = new HashMap<String, Object>();
+
+        BeanInfo beanInfo = Introspector.getBeanInfo(obj.getClass());
+        PropertyDescriptor[] propertyDescriptors = beanInfo.getPropertyDescriptors();
+        for (PropertyDescriptor property : propertyDescriptors) {
+            String key = property.getName();
+            if (key.compareToIgnoreCase("class") == 0) {
+                continue;
+            }
+            Method getter = property.getReadMethod();
+            Object value = getter!=null ? getter.invoke(obj) : null;
+            map.put(key, value);
+        }
+
+        return map;
+    }
+
+    public static Object mapToObject3(Map<String, Object> map, Class<?> beanClass) throws Exception {
         if (map == null)
             return null;
 
@@ -48,7 +115,7 @@ public class Object2Map {
         return obj;
     }
 
-    public static Map<String, Object> objectToMap(Object obj) throws Exception {
+    public static Map<String, Object> objectToMap3(Object obj) throws Exception {
         if(obj == null){
             return null;
         }
