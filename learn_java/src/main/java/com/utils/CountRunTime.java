ef4aa46b90e19d34063274b0611ed85d968a5a4a
yxrswx <yxrswx@163.com>
2017-12-06 09:28:41 +0800

    date

--- learn_java/src/main/java/com/utils/CountRunTime.java
+++ learn_java/src/main/java/com/utils/CountRunTime.java
@@ -11,10 +11,9 @@ public class CountRunTime {
     public static void main(String[] args) {
         long startTime = System.currentTimeMillis();
 
-        for (int i = 0; i < 100000000; i++) {
-            i++;
-        }
-        //执行方法
+
+
+
         long endTime = System.currentTimeMillis();
         float excTime = (float) (endTime - startTime) / 1000;
         System.out.println("执行时间：" + excTime + "s");
