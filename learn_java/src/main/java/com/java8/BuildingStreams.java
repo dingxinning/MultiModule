6671fea0e23aee9794a14a7a53f3ff8e09ff4745
yxrswx <yxrswx@163.com>
2017-12-08 14:42:26 +0800

    构建流

--- learn_java/src/main/java/com/java8/BuildingStreams.java
+++ learn_java/src/main/java/com/java8/BuildingStreams.java
@@ -12,68 +12,83 @@ import java.nio.file.*;
  * 5.7 构建流
  */
 public class BuildingStreams {
-    public static void main(String[] args) throws Exception{
-        // Stream.of
-        Stream<String> stream = Stream.of("Java 8", "Lambdas", "In", "Action");
-        stream.map(String::toUpperCase).forEach(System.out::println);
+    public static void main(String[] args) throws Exception {
 
-        // Stream.empty
+        // Stream.of  通过显式值构建流
+        Stream<String> stream = Stream.of("Java 8", "Lambdas", "In", "Action");
+        stream.map(String::toUpperCase).forEach(s -> System.out.print(s + ", "));
+        System.out.println();
+
+        // Stream.empty  获得空流
         Stream<String> emptyStream = Stream.empty();
 
-        // Arrays.stream
+        // Arrays.stream 由数组构建流
         int[] numbers = {2, 3, 5, 7, 11, 13};
         System.out.println(Arrays.stream(numbers).sum());
 
-        // Stream.iterate
+        // Stream.iterate  函数生成流，创建无限流
+        // 1. 生成 一个所有正偶数的流
         Stream.iterate(0, n -> n + 2)
                 .limit(10)
-                .forEach(System.out::println);
+                .forEach(s -> System.out.print(s + ", "));
+        System.out.println();
 
-        // fibonnaci with iterate
-        Stream.iterate(new int[]{0, 1}, t -> new int[]{t[1],t[0] + t[1]})
+        // 2. 生成 斐波那契数列
+        Stream.iterate(new int[]{0, 1}, t -> new int[]{t[1], t[0] + t[1]})
                 .limit(10)
                 .forEach(t -> System.out.println("(" + t[0] + ", " + t[1] + ")"));
 
-        Stream.iterate(new int[]{0, 1}, t -> new int[]{t[1],t[0] + t[1]})
-                .limit(10)
-                . map(t -> t[0])
-                .forEach(System.out::println);
+        Stream.iterate(new int[]{0, 1}, t -> new int[]{t[1], t[0] + t[1]})
+                .limit(20)
+                .map(t -> t[0])
+                .forEach(s -> System.out.print(s + ", "));
+        System.out.println();
 
-        // random stream of doubles with Stream.generate
+        // Stream.generate  生成
+        // 0~1的随机数  无限流
         Stream.generate(Math::random)
-                .limit(10)
-                .forEach(System.out::println);
-
-        // stream of 1s with Stream.generate
-        IntStream.generate(() -> 1)
                 .limit(5)
                 .forEach(System.out::println);
 
-        IntStream.generate(new IntSupplier(){
-            public int getAsInt(){
+        // 函数式接口  ，全是 1 的无限流
+        IntStream.generate(() -> 1)
+                .limit(10)
+                .forEach(s -> System.out.print(s + ", "));
+        System.out.println();
+
+        // 实现  IntSupplier  ，全是 2 的无限流
+        IntStream.generate(new IntSupplier() {
+            public int getAsInt() {
                 return 2;
             }
-        }).limit(5)
-                .forEach(System.out::println);
+        })
+                .limit(10)
+                .forEach(s -> System.out.print(s + ", "));
+        System.out.println();
 
-
-        IntSupplier fib = new IntSupplier(){
+        // 实现 IntSupplier ，构建斐波那契数列 无限流
+        IntSupplier fib = new IntSupplier() {
             private int previous = 0;
             private int current = 1;
-            public int getAsInt(){
+
+            public int getAsInt() {
                 int nextValue = this.previous + this.current;
                 this.previous = this.current;
                 this.current = nextValue;
                 return this.previous;
             }
         };
-        IntStream.generate(fib).limit(10).forEach(System.out::println);
+        IntStream.generate(fib)
+                .limit(20)
+                .forEach(s -> System.out.print(s + ", "));
+        System.out.println();
 
-        long uniqueWords = Files.lines(Paths.get("lambdasinaction/chap5/data.txt"), Charset.defaultCharset())
-                .flatMap(line -> Arrays.stream(line.split(" ")))
-                .distinct()
-                .count();
-
-        System.out.println("There are " + uniqueWords + " unique words in data.txt");
+        // 从文件生成流
+//        long uniqueWords = Files.lines(Paths.get("data.txt"), Charset.defaultCharset())
+//                .flatMap(line -> Arrays.stream(line.split(" ")))
+//                .distinct()
+//                .count();
+//
+//        System.out.println("There are " + uniqueWords + " unique words in data.txt");
     }
 }
