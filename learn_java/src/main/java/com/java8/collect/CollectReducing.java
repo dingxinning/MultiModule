ee30b9dd1dfb4346947cdbde2188904f16933ea5
yxrswx <yxrswx@163.com>
2017-12-08 18:23:50 +0800

    汇总和归约

--- learn_java/src/main/java/com/java8/collect/CollectReducing.java
+++ learn_java/src/main/java/com/java8/collect/CollectReducing.java
@@ -7,28 +7,41 @@ import static java.util.stream.Collectors.reducing;
  * Created by Wuxiang on 2017/12/8.
  *
  * Java8 In Action 第六章 用流收集数据
- * 6.2 归约和汇总
+ * 6.2 归约和汇总  2 广义
+ *
+ * Collectors.reducing  所有特殊情况的一般化
  */
 public class CollectReducing {
+
     public static void main(String[] args) {
+        // 利用 Collectors.reducing 求和
         System.out.println("Total calories in menu: " + calculateTotalCalories());
         System.out.println("Total calories in menu: " + calculateTotalCaloriesWithMethodReference());
         System.out.println("Total calories in menu: " + calculateTotalCaloriesWithoutCollectors());
         System.out.println("Total calories in menu: " + calculateTotalCaloriesUsingSum());
     }
 
+    /*
+     * Collectors.reducing 的完整参数
+     * 1. U identity,   // 归约操作的初始值
+     * 2. Function<? super T, ? extends U> mapper,  // 转换函数
+     * 3. BinaryOperator<U> op    // 操作
+     */
     private static int calculateTotalCalories() {
         return menu.stream().collect(reducing(0, Dish::getCalories, (Integer i, Integer j) -> i + j));
     }
 
+    // 使用 累加函数 Integer::sum  -> 代替 Lambda表达式
     private static int calculateTotalCaloriesWithMethodReference() {
         return menu.stream().collect(reducing(0, Dish::getCalories, Integer::sum));
     }
 
+    // 或者 利用 stream的reduce()操作，最后 得到一个 Optional<Integer> ，利用get()提取值
     private static int calculateTotalCaloriesWithoutCollectors() {
         return menu.stream().map(Dish::getCalories).reduce(Integer::sum).get();
     }
 
+    // 或者 更简洁的写法
     private static int calculateTotalCaloriesUsingSum() {
         return menu.stream().mapToInt(Dish::getCalories).sum();
     }
--- learn_java/src/main/java/com/java8/collect/CollectSummarizing.java
+++ learn_java/src/main/java/com/java8/collect/CollectSummarizing.java
@@ -10,7 +10,7 @@ import static com.java8.collect.Dish.menu;
  * Created by Wuxiang on 2017/12/8.
  *
  * Java8 In Action 第六章 用流收集数据
- * 6.2 归约和汇总
+ * 6.2 归约和汇总  1 狭义
  */
 public class CollectSummarizing {
     public static void main(String[] args) {
@@ -24,11 +24,12 @@ public class CollectSummarizing {
         System.out.println("Short menu comma separated: " + getShortMenuCommaSeparated());
     }
 
-    // 利用 counting工厂 返回的收集器，计算流中元素的个数
+    // Collectors.counting 计算流中元素的个数
     private static long howManyDishes() {
         return menu.stream().collect(counting());
     }
 
+    // Collectors.reducing 求最大值 (单参数)
     private static Dish findMostCaloricDish() {
         return menu.stream().collect(reducing((d1, d2) -> d1.getCalories() > d2.getCalories() ? d1 : d2)).get();
     }
@@ -41,18 +42,22 @@ public class CollectSummarizing {
         return menu.stream().collect(reducing(moreCaloricOf)).get();
     }
 
+    // Collectors.summingInt 汇总 求和  (summingDouble & summingLong 效果完全相同)
     private static int calculateTotalCalories() {
         return menu.stream().collect(summingInt(Dish::getCalories));
     }
 
+    // Collectors.averagingInt 汇总 求平均
     private static Double calculateAverageCalories() {
         return menu.stream().collect(averagingInt(Dish::getCalories));
     }
 
+    // Collectors.summarizingInt 所有的汇总信息 包含 count=9, sum=4300, min=120, average=477.777778, max=800
     private static IntSummaryStatistics calculateMenuStatistics() {
         return menu.stream().collect(summarizingInt(Dish::getCalories));
     }
 
+    // Collectors.joining 连接字符串
     private static String getShortMenu() {
         return menu.stream().map(Dish::getName).collect(joining());
     }
