a53887c1ce823346a8a6c7a0f88e23a6fad7a3ad
yxrswx <yxrswx@163.com>
2018-01-07 12:40:36 +0800

    优化 截取功能实现

--- learn_java/src/main/java/com/strings/TestString.java
+++ learn_java/src/main/java/com/strings/TestString.java
@@ -8,37 +8,61 @@ import java.util.List;
  *
  * 根据 number 分割字符串
  *
- * 最后一个多的元素的长度在2以内，归到前一个中
+ * 基础功能：根据字符串的长度，进行截取
+ *
+ * 升级版功能：  根据中文和英文长度的不同，进行截取，
+ *              而不是之前简单的根据字符个数进行截取
  */
 public class TestString {
     public static void main(String[] args) {
-        int number = 5;
-        String string = "12345678901234512";
+        int maxLength = 13;  // 一行最大能容纳的 字符个数 （中文）
+        int beyondLength = 1; // 最后一行必须超过的字符数
+
+        String string = "19小时总入量为2200ml，其中输液量为1900ml，输血量为300ml。19小时总出量为3100ml，均为尿量。";
+
+        int totalSize = string.length();
+        int totalLength = getLength(string);
+        int totalMax = 2 * maxLength;
+
         List<String> list = new ArrayList<>();
-
         int startIndex = 0;
-        int endIndex = number;
-        if (string.length() > 0) {
-            for (int i = 0; i < string.length() / number + 1; i++) {
-                if (endIndex > string.length()) {
-                    endIndex = string.length();
-                }
+        int endIndex = 1;
 
+        if (totalSize > 0) {
+            for (int i = 0; i < totalLength / totalMax + 1; i++) {
+
+                if (endIndex > totalSize) {
+                    endIndex = totalSize;
+                }
                 if (startIndex == endIndex) {
                     break;
                 }
 
-                list.add(string.substring(startIndex, endIndex));
+                for (int j = 0; j < totalMax; j++) {
+                    String subStr = string.substring(startIndex, endIndex);
+                    // 防止越界，最后一列进行处理，并将最后一段也添加到list中
+                    if (endIndex == totalSize) {
+                        list.add(subStr);
+                        break;
+                    }
+
+                    // 每段最后，不是英文字符，就是中文字符
+                    if (getLength(subStr) == totalMax || getLength(subStr) == totalMax + 1) {
+                        list.add(subStr);
+                        break;
+                    }else{
+                        endIndex ++;
+                    }
+                }
 
                 startIndex = endIndex;
-                endIndex += number;
+                endIndex += 1;
             }
         }
 
-        System.out.println(list);
 
         if (list.size() > 1) {
-            if (list.get(list.size() - 1).length() <= 2) {
+            if (list.get(list.size() - 1).length() <= beyondLength) {
                 list.set(list.size() - 2,
                         list.get(list.size() - 2) +
                                 list.get(list.size() - 1));
@@ -46,6 +70,28 @@ public class TestString {
             }
         }
 
-        System.out.println(list);
+        list.forEach(s -> {
+            System.out.println("#" + s + "#");
+        });
+    }
+
+    // 计算字符串的长度，中文算两个，英文算一个
+    public static int getLength(String string) {
+        int len = 0;
+        for (int i = 0; i < string.length(); i++) {
+            if (string.charAt(i) > 127 || string.charAt(i) == 94) {
+                len += 2;
+            } else {
+                len++;
+            }
+        }
+        return len;
+    }
+
+    // 获取unicode编码  类似于 javascript中的 charCodeAt
+    public static Integer getUnicode(char c){
+        String returnUniCode;
+        returnUniCode=String.valueOf((int)c);
+        return Integer.parseInt(returnUniCode);
     }
 }
