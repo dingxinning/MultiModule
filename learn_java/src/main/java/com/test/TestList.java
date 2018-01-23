c6e024ddd781ed98d37a0c32a1a07f9f96d1d02e
yxrswx <yxrswx@163.com>
2018-01-23 14:49:39 +0800

    sub

--- learn_java/src/main/java/com/test/TestList.java
+++ learn_java/src/main/java/com/test/TestList.java
@@ -1,6 +1,7 @@
 package com.test;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -20,5 +21,23 @@ public class TestList {
         System.out.println(list.size());
         list.clear();
         System.out.println(list.size());
+
+
+        List<String> strings = Arrays.asList("1","2","3","4","5","1","2","3","4","5","1","2","3","4","5","1","2","3","4","5","4","5");
+
+        List<List<String>> newStrings = new ArrayList<>();
+        if (strings.size() > 7) {
+            for (int i = 0; i < strings.size() / 7 + 1; i++) {
+                int start = i * 7;
+                int end = start + 7;
+                if(end > strings.size()){
+                    end = strings.size();
+                }
+                newStrings.add(strings.subList(start, end));
+                System.out.println(newStrings);
+            }
+        }else{
+            newStrings.add(strings);
+        }
     }
 }
