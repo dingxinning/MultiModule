6ccf77704bc789b0e4ee3e5b01525664f6021230
yxrswx <yxrswx@163.com>
2018-02-04 21:38:50 +0800

    model duck

--- head_first_design_patterns/src/main/java/com/multi/project/c1_duck/TestMallardDuck.java
+++ head_first_design_patterns/src/main/java/com/multi/project/c1_duck/TestMallardDuck.java
@@ -2,6 +2,8 @@ package com.multi.project.c1_duck;
 
 /**
  * Created by Wuxiang on 2018/2/4
+ *
+ * 创建一个实例，会飞，会叫
  */
 public class TestMallardDuck {
     public static void main(String[] args) {
--- head_first_design_patterns/src/main/java/com/multi/project/c1_duck/TestModelDuck.java
+++ head_first_design_patterns/src/main/java/com/multi/project/c1_duck/TestModelDuck.java
@@ -2,6 +2,9 @@ package com.multi.project.c1_duck;
 
 /**
  * Created by Wuxiang on 2018/2/4
+ *
+ * 创建模型鸭，不会飞，会叫
+ * 动态设定行为，使其会飞
  */
 public class TestModelDuck {
     public static void main(String[] args) {
