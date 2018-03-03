84c2ab25aeee884d6e27967a90e7c71beaabe5b9
yxrswx <yxrswx@163.com>
2018-03-03 16:17:13 +0800

    synchronized block

---
+++ learn_java/src/main/java/com/thread_learn/synchronized_block/ExampleOther.java
@@ -0,0 +1,18 @@
+package com.thread_learn.synchronized_block;
+
+/**
+ * Created by Wuxiang on 2018/3/3
+ */
+public class ExampleOther {
+    public static void main(String[] args) {
+
+        // 两个线程引用了两个不同的Counter实例，那么他们可以同时调用add()方法。
+        Counter counterA = new Counter();
+        Counter counterB = new Counter();
+        Thread  threadA = new CounterThread(counterA);
+        Thread  threadB = new CounterThread(counterB);
+
+        threadA.start();
+        threadB.start();
+    }
+}
