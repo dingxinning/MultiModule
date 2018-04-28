3c975b612ef5050fb1b33820f316eed1bef3b65f
wuxiang <wuxiang@fenqi.im>
2018-04-28 16:33:40 +0800

    webmagic

---
+++ learn_java/src/main/java/com/test/TestByte.java
@@ -0,0 +1,14 @@
+package com.test;
+
+/**
+ * Created by Wuxiang on 2018/4/27
+ */
+public class TestByte {
+    public static final Byte STATUS_NOT_SYNC = 0;
+
+    public static void main(String[] args) {
+        System.out.println(STATUS_NOT_SYNC);
+
+        System.out.println(Integer.valueOf(STATUS_NOT_SYNC));
+    }
+}
