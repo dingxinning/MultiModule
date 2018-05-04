57a51f1038bf99680e82678705b0a02aa1a8d4bf
yxrswx <yxrswx@163.com>
2018-05-04 11:26:01 +0800

    SHA-256加密

--- learn_java/src/main/java/com/utils/Object2Map.java
+++ learn_java/src/main/java/com/utils/Object2Map.java
@@ -1,137 +1,137 @@
-package com.utils;
-
-import com.cglib.LeapRole;
-import com.mongodb.util.JSON;
-
-import java.beans.BeanInfo;
-import java.beans.Introspector;
-import java.beans.PropertyDescriptor;
-import java.lang.reflect.Field;
-import java.lang.reflect.Method;
-import java.lang.reflect.Modifier;
-import java.util.HashMap;
-import java.util.Map;
-
-/**
- * Created by Wuxiang on 2017/11/1.
- * Object 与 Map 之间的转换
- * 仅限于 Object 拥有java的实体类结构
- *
- * 参考：http://www.open-open.com/code/view/1423280939826
- *
- * 方法1 利用 beanutils
- * 方法2 利用 Introspector 进行转换
- * 方法3 利用 反射
- */
-public class Object2Map {
-    public static void main(String[] args) throws Exception {
-        LeapRole leapRole = new LeapRole();
-        leapRole.setDef1("1");
-        leapRole.setDef2("2");
-        Object object = (Object) leapRole;
-        System.out.println(object);
-
-        // Object ---> Map
-        Map<String, Object> map = objectToMap3(object);
-        System.out.println(map);
-
-        // Map ---> Object
-        System.out.println(mapToObject3(map, leapRole.getClass()));
-
-        String json = JSON.serialize(map);
-        System.out.println(json);
-    }
-
-    public static Object mapToObject1(Map<String, Object> map, Class<?> beanClass) throws Exception {
-        if (map == null)
-            return null;
-
-        Object obj = beanClass.newInstance();
-
-        org.apache.commons.beanutils.BeanUtils.populate(obj, map);
-
-        return obj;
-    }
-
-    public static Map<?, ?> objectToMap1(Object obj) {
-        if(obj == null)
-            return null;
-
-        return new org.apache.commons.beanutils.BeanMap(obj);
-    }
-
-    public static Object mapToObject2(Map<String, Object> map, Class<?> beanClass) throws Exception {
-        if (map == null)
-            return null;
-
-        Object obj = beanClass.newInstance();
-
-        BeanInfo beanInfo = Introspector.getBeanInfo(obj.getClass());
-        PropertyDescriptor[] propertyDescriptors = beanInfo.getPropertyDescriptors();
-        for (PropertyDescriptor property : propertyDescriptors) {
-            Method setter = property.getWriteMethod();
-            if (setter != null) {
-                setter.invoke(obj, map.get(property.getName()));
-            }
-        }
-
-        return obj;
-    }
-
-    public static Map<String, Object> objectToMap2(Object obj) throws Exception {
-        if(obj == null)
-            return null;
-
-        Map<String, Object> map = new HashMap<String, Object>();
-
-        BeanInfo beanInfo = Introspector.getBeanInfo(obj.getClass());
-        PropertyDescriptor[] propertyDescriptors = beanInfo.getPropertyDescriptors();
-        for (PropertyDescriptor property : propertyDescriptors) {
-            String key = property.getName();
-            if (key.compareToIgnoreCase("class") == 0) {
-                continue;
-            }
-            Method getter = property.getReadMethod();
-            Object value = getter!=null ? getter.invoke(obj) : null;
-            map.put(key, value);
-        }
-
-        return map;
-    }
-
-    public static Object mapToObject3(Map<String, Object> map, Class<?> beanClass) throws Exception {
-        if (map == null)
-            return null;
-
-        Object obj = beanClass.newInstance();
-
-        Field[] fields = obj.getClass().getDeclaredFields();
-        for (Field field : fields) {
-            int mod = field.getModifiers();
-            if(Modifier.isStatic(mod) || Modifier.isFinal(mod)){
-                continue;
-            }
-
-            field.setAccessible(true);
-            field.set(obj, map.get(field.getName()));
-        }
-
-        return obj;
-    }
-
-    public static Map<String, Object> objectToMap3(Object obj) throws Exception {
-        if(obj == null){
-            return null;
-        }
-
-        Map<String, Object> map = new HashMap<String, Object>();
-
-        Field[] declaredFields = obj.getClass().getDeclaredFields();
-        for (Field field : declaredFields) {
-            field.setAccessible(true);
-            map.put(field.getName(), field.get(obj));
-        }
-
-        return map;
-    }
-}
+//package com.utils;
+//
+//import com.cglib.LeapRole;
+//import com.mongodb.util.JSON;
+//
+//import java.beans.BeanInfo;
+//import java.beans.Introspector;
+//import java.beans.PropertyDescriptor;
+//import java.lang.reflect.Field;
+//import java.lang.reflect.Method;
+//import java.lang.reflect.Modifier;
+//import java.util.HashMap;
+//import java.util.Map;
+//
+///**
+// * Created by Wuxiang on 2017/11/1.
+// * Object 与 Map 之间的转换
+// * 仅限于 Object 拥有java的实体类结构
+// *
+// * 参考：http://www.open-open.com/code/view/1423280939826
+// *
+// * 方法1 利用 beanutils
+// * 方法2 利用 Introspector 进行转换
+// * 方法3 利用 反射
+// */
+//public class Object2Map {
+//    public static void main(String[] args) throws Exception {
+//        LeapRole leapRole = new LeapRole();
+//        leapRole.setDef1("1");
+//        leapRole.setDef2("2");
+//        Object object = (Object) leapRole;
+//        System.out.println(object);
+//
+//        // Object ---> Map
+//        Map<String, Object> map = objectToMap3(object);
+//        System.out.println(map);
+//
+//        // Map ---> Object
+//        System.out.println(mapToObject3(map, leapRole.getClass()));
+//
+//        String json = JSON.serialize(map);
+//        System.out.println(json);
+//    }
+//
+//    public static Object mapToObject1(Map<String, Object> map, Class<?> beanClass) throws Exception {
+//        if (map == null)
+//            return null;
+//
+//        Object obj = beanClass.newInstance();
+//
+//        org.apache.commons.beanutils.BeanUtils.populate(obj, map);
+//
+//        return obj;
+//    }
+//
+//    public static Map<?, ?> objectToMap1(Object obj) {
+//        if(obj == null)
+//            return null;
+//
+//        return new org.apache.commons.beanutils.BeanMap(obj);
+//    }
+//
+//    public static Object mapToObject2(Map<String, Object> map, Class<?> beanClass) throws Exception {
+//        if (map == null)
+//            return null;
+//
+//        Object obj = beanClass.newInstance();
+//
+//        BeanInfo beanInfo = Introspector.getBeanInfo(obj.getClass());
+//        PropertyDescriptor[] propertyDescriptors = beanInfo.getPropertyDescriptors();
+//        for (PropertyDescriptor property : propertyDescriptors) {
+//            Method setter = property.getWriteMethod();
+//            if (setter != null) {
+//                setter.invoke(obj, map.get(property.getName()));
+//            }
+//        }
+//
+//        return obj;
+//    }
+//
+//    public static Map<String, Object> objectToMap2(Object obj) throws Exception {
+//        if(obj == null)
+//            return null;
+//
+//        Map<String, Object> map = new HashMap<String, Object>();
+//
+//        BeanInfo beanInfo = Introspector.getBeanInfo(obj.getClass());
+//        PropertyDescriptor[] propertyDescriptors = beanInfo.getPropertyDescriptors();
+//        for (PropertyDescriptor property : propertyDescriptors) {
+//            String key = property.getName();
+//            if (key.compareToIgnoreCase("class") == 0) {
+//                continue;
+//            }
+//            Method getter = property.getReadMethod();
+//            Object value = getter!=null ? getter.invoke(obj) : null;
+//            map.put(key, value);
+//        }
+//
+//        return map;
+//    }
+//
+//    public static Object mapToObject3(Map<String, Object> map, Class<?> beanClass) throws Exception {
+//        if (map == null)
+//            return null;
+//
+//        Object obj = beanClass.newInstance();
+//
+//        Field[] fields = obj.getClass().getDeclaredFields();
+//        for (Field field : fields) {
+//            int mod = field.getModifiers();
+//            if(Modifier.isStatic(mod) || Modifier.isFinal(mod)){
+//                continue;
+//            }
+//
+//            field.setAccessible(true);
+//            field.set(obj, map.get(field.getName()));
+//        }
+//
+//        return obj;
+//    }
+//
+//    public static Map<String, Object> objectToMap3(Object obj) throws Exception {
+//        if(obj == null){
+//            return null;
+//        }
+//
+//        Map<String, Object> map = new HashMap<String, Object>();
+//
+//        Field[] declaredFields = obj.getClass().getDeclaredFields();
+//        for (Field field : declaredFields) {
+//            field.setAccessible(true);
+//            map.put(field.getName(), field.get(obj));
+//        }
+//
+//        return map;
+//    }
+//}
