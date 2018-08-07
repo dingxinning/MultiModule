b7e57293f9a6e0c3c534c7c63e3aa0e39a9cc9db
wuxiang <yxrswx@163.com>
2018-08-07 15:35:55 +0800

    date

--- learn_java/src/main/java/com/test/TestDate.java
+++ learn_java/src/main/java/com/test/TestDate.java
@@ -2,7 +2,9 @@ package com.test;
 
 import com.utils.DateConversion;
 
+import java.text.DateFormat;
 import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 /**
@@ -17,6 +19,23 @@ public class TestDate {
             System.out.println(date1);
 
         } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+
+        String dateStr;
+        Date date = new Date();
+        DateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+        DateFormat sdf2 = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.S");
+        DateFormat sdf3 = new SimpleDateFormat("hh:mm:ss a E z");
+        try {
+            dateStr = sdf.format(date);
+            System.out.println(dateStr);
+            dateStr = sdf2.format(date);
+            System.out.println(dateStr);
+            dateStr = sdf3.format(date);
+            System.out.println(dateStr);
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
