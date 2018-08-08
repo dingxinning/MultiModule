6d8a1f41db0e2600f4caa98208401c2450095e26
wuxiang <yxrswx@163.com>
2018-08-08 17:23:30 +0800

    lombock 失效？

--- learn_java/src/main/java/com/resttemplate/Response.java
+++ learn_java/src/main/java/com/resttemplate/Response.java
@@ -1,13 +1,10 @@
 package com.resttemplate;
 
-import lombok.Data;
-
 import java.util.Date;
 
 /**
  * Created by Wuxiang on 2018/5/31
  */
-@Data
 public class Response {
 
     private String userId;
@@ -17,4 +14,46 @@ public class Response {
     private Date responseDate;
 
     private Date requestDate;
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getOneCode() {
+        return oneCode;
+    }
+
+    public void setOneCode(String oneCode) {
+        this.oneCode = oneCode;
+    }
+
+    public Date getResponseDate() {
+        return responseDate;
+    }
+
+    public void setResponseDate(Date responseDate) {
+        this.responseDate = responseDate;
+    }
+
+    public Date getRequestDate() {
+        return requestDate;
+    }
+
+    public void setRequestDate(Date requestDate) {
+        this.requestDate = requestDate;
+    }
+
+    @Override
+    public String toString() {
+        return "Response{" +
+                "userId='" + userId + '\'' +
+                ", oneCode='" + oneCode + '\'' +
+                ", responseDate=" + responseDate +
+                ", requestDate=" + requestDate +
+                '}';
+    }
 }
